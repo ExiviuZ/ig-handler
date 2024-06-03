28263cfa6689be1d6daedb0c43bed5b8ff9c3e5c
Mark Angel Papio <papio.markangel788@gmail.com>
2024-06-03 15:17:18 +0800

    added a path

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/redirect-uri', async (req, res) => {
     const code = req.query.code;
     res.send(code);
 });
 
-app.listen(3000, () => {
+app.get('/', async (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
     console.log('App is listening on port 3000');
 });
