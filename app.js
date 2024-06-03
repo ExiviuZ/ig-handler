7d8280600dbcd8ac848238f1b4112b48e3b0d168
Mark Angel Papio <papio.markangel788@gmail.com>
2024-06-03 15:41:15 +0800

    styled

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const path = require('path'); // For path manipulation
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4321;
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
 app.get('/redirect-uri', async (req, res) => {
     const code = req.query.code;
-    res.send(code);
+    res.render('token', code);
 });
 
 app.get('/', async (req, res) => {
-    res.send('Hello World');
+    const data = { title: 'My Dynamic Title' };
+    res.render('index', data);
 });
 
 app.listen(port, () => {
-    console.log('App is listening on port 3000');
+    console.log(`App is listening on port ${port}`);
 });
