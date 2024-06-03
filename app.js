ade485e9f252cff6829b94c8158ad494fe8b3ba6
Mark Angel Papio <papio.markangel788@gmail.com>
2024-06-03 16:41:47 +0800

    fixed

--- app.js
+++ app.js
@@ -10,12 +10,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/redirect-uri', async (req, res) => {
-    // const code = req.query.code;
-    const code =
-        'AQDYRAjRP9Z8RY3BA5ZR4lO0mRShVHncq8JqXOguZWbood66SUQMrocr9eNzTUNfja_2H46UpF8YI_ncsOmGFWQUkkFX2i2IXvLReAT_VWVINoBSzREH1f96g58RmeGNR1ONMSsJ0d3Qwjm8nDcJ-DT8XyR9gucAe7rFG4n4S6eFJE_mslRtWp9BLj0IFYDFSLc1BGfYaHu7D_nG9u0nQGV6gJaQXOhW4JTZtfCeIpAO_A';
-    res.render('token', {
-        code,
-    });
+    const code = req.query.code;
+
+    res.render('token', { code });
 });
 
 app.get('/', async (req, res) => {
